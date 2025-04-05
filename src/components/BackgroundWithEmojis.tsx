36e9cb5cf6f737fd4d0e1c87bc5a63b4954d5478
mindvincibledev <mindvincibledev@gmail.com>
2025-04-05 08:58:31 -0500

    change to transparent

--- src/components/BackgroundWithEmojis.tsx
+++ src/components/BackgroundWithEmojis.tsx
@@ -13,55 +13,16 @@ interface BackgroundWithEmojisProps {
 const BackgroundWithEmojis: FC<BackgroundWithEmojisProps> = ({
   children,
   showWaves = true,
-  showBlobs = true,
-}) => {
-  return (
-    <div className="min-h-screen font-poppins relative galaxy-bg overflow-hidden">
-      {/* Animated Blobs */}
-      {showBlobs && (
-        <>
-          <div className="blob blob-1"></div>
-          <div className="blob blob-2"></div>
-          <div className="blob blob-3"></div>
-        </>
-      )}
+}) => (
+  <div className="min-h-screen font-poppins relative galaxy-bg overflow-hidden">
+    <AnimatedEmoji icon="smile" color="#FC68B3" size="md" position={{ top: "40px", left: "7%" }} />
+    <AnimatedEmoji icon="sparkles" color="#F5DF4D" size="lg" position={{ top: "60px", right: "10%" }} delay={1.5} />
+    <AnimatedEmoji icon="brain" color="#3DFDFF" size="sm" position={{ bottom: "32px", left: "15%" }} delay={2} />
+    <AnimatedEmoji icon="message" color="#2AC20E" size="md" position={{ bottom: "80px", right: "18%" }} delay={1} />
+    {showWaves && <Wave />}
+    <div className="relative z-10">{children}</div>
+  </div>
+);
 
-      {/* Floating Emoji Icons */}
-      <AnimatedEmoji
-        icon="smile"
-        color="#FC68B3"
-        size="md"
-        position={{ top: "40px", left: "7%" }}
-      />
-      <AnimatedEmoji
-        icon="sparkles"
-        color="#F5DF4D"
-        size="lg"
-        position={{ top: "60px", right: "10%" }}
-        delay={1.5}
-      />
-      <AnimatedEmoji
-        icon="brain"
-        color="#3DFDFF"
-        size="sm"
-        position={{ bottom: "32px", left: "15%" }}
-        delay={2}
-      />
-      <AnimatedEmoji
-        icon="message"
-        color="#2AC20E"
-        size="md"
-        position={{ bottom: "80px", right: "18%" }}
-        delay={1}
-      />
-
-      {/* Wave Background */}
-      {showWaves && <Wave />}
-
-      {/* Main Content */}
-      <div className="relative z-10">{children}</div>
-    </div>
-  );
-};
 
 export default BackgroundWithEmojis;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,19 @@
-
+// app/page.tsx
 import React from 'react';
 import { TaskProvider } from '@/context/TaskContext';
 import TaskList from '@/components/TaskList';
 import Header from '@/components/Header';
 import BackgroundWithEmojis from "@/components/BackgroundWithEmojis";
+
 export default function Home() {
   return (
     <BackgroundWithEmojis>
-      <TaskProvider>
-        <Header />
-        <TaskList />
-      </TaskProvider>
-
+      <div className="min-h-screen bg-transparent p-4 md:p-8">
+        <TaskProvider>
+          <Header />
+          <TaskList />
+        </TaskProvider>
+      </div>
     </BackgroundWithEmojis>
-    
   );
 };
-
-
