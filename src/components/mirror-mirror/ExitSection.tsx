e0c82d566e1f609c210b407f386f2b1dcdf0df65
mindvincibledev <mindvincibledev@gmail.com>
2025-04-22 20:05:17 -0500

    fb

--- src/components/mirror-mirror/ExitSection.tsx
+++ src/components/mirror-mirror/ExitSection.tsx
@@ -92,7 +92,7 @@ const ExitSection: React.FC<ExitSectionProps> = ({ onAnotherPrompt, onComplete, 
               <Button 
                 onClick={() => handleFeedback('positive')} 
                 variant="outline" 
-                className="flex flex-col items-center p-4 hover:bg-green-100 hover:border-green-300 transition-colors h-auto"
+                className="flex flex-col items-center p-4 hover:bg-yellow-50 hover:border-yellow-200 transition-colors h-auto"
                 disabled={isSubmitting}
               >
                 <div className="text-3xl mb-2">👍</div>
